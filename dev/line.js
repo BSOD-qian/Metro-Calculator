9b945fac6221349ead015ff0b7276fefcfe95c12
BSOD-qian <63780982+BSOD-qian@users.noreply.github.com>
2020-08-25 07:33:05 +0800

    1

--- dev/line.js
+++ dev/line.js
@@ -1,5 +1,5 @@
 var myCars = [
-    '1号线<br/>01A01<br/>01001<br/>92011<br/>特殊性：张江实训专用车<hr/>',
+    '<div class="mdui-chip"><span class="mdui-chip-title">1号线/01A01/01001/92011/特殊性：张江实训专用车</span></div>',
 ];
 function findone() {
     mykey = document.getElementById("mykeyword").value;
