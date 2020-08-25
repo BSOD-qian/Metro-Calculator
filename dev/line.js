e4df4dbfba330b55071184cc9ff9fb89cbb517b5
BSOD-qian <63780982+BSOD-qian@users.noreply.github.com>
2020-08-25 21:41:02 +0800

    1

--- dev/line.js
+++ dev/line.js
@@ -1,5 +1,12 @@
 var myCars = [
-    '<div class="mdui-chip"><span class="mdui-chip-title">1号线/01A01/01001/92011/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/92011/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/92022/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/92033/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/93442/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/93453/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/94102/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/94113/特殊性：张江实训专用车</span></div>',
+    '<div class="mdui-chip mdui-ripple"><span class="mdui-chip-title">1号线/01A01/01001/94121/特殊性：张江实训专用车</span></div>',
 ];
 function findone() {
     mykey = document.getElementById("mykeyword").value;
